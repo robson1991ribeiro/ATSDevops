b3fedc202e8b18c7f1b6e1ee8edb4921b03260fe
robson1991ribeiro <robson1991ribeiro@gmail.com>
2026-09-18 23:43:05 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
 import sys
 from pathlib import Path
-from unittest.mock import patch
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
@@ -10,42 +9,31 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 
-def test_home_status_code():
+def test_home():
     response = client.get("/")
     assert response.status_code == 200
 
 
-def test_windows_status_code():
+def test_windows():
     response = client.get("/windows")
     assert response.status_code == 200
 
 
-def test_rede_status_code():
+def test_rede():
     response = client.get("/rede")
     assert response.status_code == 200
 
 
-def test_home_template():
-    with patch("main.templates.TemplateResponse") as mock_template:
-        client.get("/")
-        assert mock_template.called
-        assert mock_template.call_args.kwargs["name"] == "index.html"
+def test_home_html():
+    response = client.get("/")
+    assert "text/html" in response.headers["content-type"]
 
 
-def test_windows_template():
-    with patch("main.templates.TemplateResponse") as mock_template:
-        client.get("/windows")
-        assert mock_template.called
-        assert mock_template.call_args.kwargs["name"] == "windows.html"
-
-
-def test_rede_template():
-    with patch("main.templates.TemplateResponse") as mock_template:
-        client.get("/rede")
-        assert mock_template.called
-        assert mock_template.call_args.kwargs["name"] == "rede.html"
+def test_windows_html():
+    response = client.get("/windows")
+    assert "text/html" in response.headers["content-type"]
 
 
 def test_rota_inexistente():
-    response = client.get("/abc")
+    response = client.get("/pagina-nao-existe")
     assert response.status_code == 404
